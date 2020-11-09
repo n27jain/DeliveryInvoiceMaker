40951ee419af258f59e4fbd85a7f1ea64588db0a
Naman Jain <n27jain@uwaterloo.ca>
2020-11-09 15:10:58 -0500

    Got databse to connect and work. Got basic layout for adding a product to work. Now need to add one for searching the database for items

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
+import pyrebase
 from flask import Flask 
 from flask import render_template
 from flask import redirect
 from flask import url_for
 from flask import request
+from flask import session
+from flask import config
 
+
+
+firebaseConfig = {
+    "apiKey": "AIzaSyDjRLpE5kpL82vkQbJB9wRJC6mP1iR6o7w",
+    "authDomain": "invoices-d12ae.firebaseapp.com",
+    "databaseURL": "https://invoices-d12ae.firebaseio.com",
+    "projectId": "invoices-d12ae",
+    "storageBucket": "invoices-d12ae.appspot.com",
+    "messagingSenderId": "741972661830",
+    "appId": "1:741972661830:web:2a8ecdb51a559ce5881c47",
+    "measurementId": "G-7BKRLPKX63"
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+db = firebase.database()
+
+db.child("Products").push({"name": "Thermometer", "costPrice": "10.50", "previousSalePrice": [15,20,15,16]})
 
 app = Flask(__name__)
-
+app.secret_key = "namanjain"
 # Route for handling the login page logic
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -21,19 +41,25 @@ def login():
 @app.route("/home", methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        postItem = request.form["productEntry"]
-        print("postItem is" ,  postItem)
-        return redirect( url_for("itemPage", item = postItem) )
+        item = request.form["nm"]
+        session["item"] = item
+        print("postItem is" ,  item)
+        return redirect( url_for("itemPage") )
     else:
         return render_template('main.html')
 
-@app.route("/itemPage") 
-def itemPage(item):
-    return f"<h1> item </h1>"
+@app.route("/item")
+def itemPage():
+    if "item" in session:
+        item = session["item"]
+        return f"<h1>{item}</h1>"
+    else:
+        redirect(url_for('home'))
 
-
-
-
+@app.route("/logout")
+def logout():
+    session.pop("home", None)
+    return redirect(url_for("login"))
 
 
 
