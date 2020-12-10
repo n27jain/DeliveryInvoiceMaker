69c6d662a7f0a27563b8ea6e6b4046bfc98803b3
Naman Jain <n27jain@uwaterloo.ca>
2020-12-10 01:02:19 -0500

    refactor done

--- Item.py
+++
@@ -1,8 +0,0 @@
-class Item:
-    def __init__(self, key, name, costPrice, previousSalePrice , notes, quantity):
-        self.key = key
-        self.name = name
-        self.costPrice = costPrice
-        self.previousSalePrice = previousSalePrice
-        self.notes = notes
-        self.quantity = quantity
