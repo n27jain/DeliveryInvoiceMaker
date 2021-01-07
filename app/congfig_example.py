abfc684647f11122b42fec39728636d307c15643
Naman Jain <n27jain@uwaterloo.ca>
2021-01-07 15:17:26 -0500

    added example config file

---
+++ app/congfig_example.py
@@ -0,0 +1,20 @@
+config = dict(
+    secret_key = "<enter secret key>",
+
+    companyName ="", 
+    companyAddress ="",
+    companyTel ="",
+    companyFax ="",
+    companyEmail ="",
+    companyNote = "",
+
+    #firebase config
+    apiKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    databaseURL = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    projectId = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    storageBucket = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    appId = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    measurementId = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+)
